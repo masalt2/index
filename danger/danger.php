be435f22f2285dc7b2e23081aad44664bea4c5cf
masalt2 <122125545+masalt2@users.noreply.github.com>
2023-03-09 09:52:44 -0800

    Create danger.php

---
+++ danger/danger.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+<body>
+<br><br><br><br><br><br><br><br>
+<?php
+$t = rand(1, 51);
+if ($t == 1) {
+  echo "as lotus flowers";
+} elseif ($t == 2) {
+  echo "get off my case";
+} elseif ($t == 3) {
+  echo "if you'd been a dog";
+} elseif ($t == 4) {
+  echo "a universal sigh";
+} elseif ($t == 5) {
+  echo "this is my way of saying goodbye";
+} elseif ($t == 6) {
+  echo "all i want is a moon upon a stick";
+} elseif ($t == 7) {
+  echo "the would've drowned you at birth";
+} elseif ($t == 8) {
+  echo "because i can't do it face to face";
+} elseif ($t == 9) {
+  echo "i'm a reasonable man";
+} elseif ($t == 10) {
+  echo "catch da mouse";
+} elseif ($t == 11) {
+  echo "all you have to do is say";
+} elseif ($t == 12) {
+  echo "broken hearts";
+} elseif ($t == 13) {
+  echo "you do it to yourself";
+} elseif ($t == 14) {
+  echo "https://citizeninsane.eu/";
+} elseif ($t == 15) {
+  echo "it was just a laugh";
+} elseif ($t == 16) {
+  echo "jigsaw falling into place";
+} elseif ($t == 17) {
+  echo "this kind of thin thin thin thin thin thing";
+} elseif ($t == 18) {
+  echo "so there is nothing to explain";
+} elseif ($t == 19) {
+  echo "come to your senses (somehow)";
+} elseif ($t == 20) {
+  echo "regard each other as you pass";
+} elseif ($t == 21) {
+  echo "this just feels like spinning plates";
+} elseif ($t == 22) {
+  echo "she looks back you look back";
+} elseif ($t == 23) {
+  echo "its a terrible shame";
+} elseif ($t == 24) {
+  echo "not just once, not just twice";
+} elseif ($t == 25) {
+  echo "its a terrible shame i know";
+} elseif ($t == 26) {
+  echo "youre being took for a ride";
+} elseif ($t == 27) {
+  echo "the raindrops";
+} elseif ($t == 28) {
+  echo "plain old and easy";
+} elseif ($t == 29) {
+  echo "kinetic kinetic kinetic kinetic kinetic kinetic kinetic kinetic kinetic kinetic kinetic kinetic kinetic kinetic kinetic kinetic kinetic kinetic kinetic kinetic kinetic kinetic kinetic kinetic kinetic kinetic kinetic kinetic";
+} elseif ($t == 30) {
+  echo "couldn't wave a white flag";
+} elseif ($t == 31) {
+  echo "a penny for your thoughts, my love";
+} elseif ($t == 32) {
+  echo "dance, you f*cker";
+} elseif ($t == 33) {
+  echo "flan in the face";
+} elseif ($t == 34) {
+  echo "just to see what if";
+} elseif ($t == 35) {
+  echo "just to see what is";
+} elseif ($t == 36) {
+  echo "they possess me";
+} elseif ($t == 37) {
+  echo "they possess me";
+} elseif ($t == 38) {
+  echo "they possess me";
+} elseif ($t == 39) {
+  echo "i'm tryna be a thought killer";
+} elseif ($t == 40) {
+  echo "oh no, pop is dead; long live pop";
+} elseif ($t == 41) {
+  echo "you can taste it but it will not come";
+} elseif ($t == 42) {
+  echo "i am born again";
+} elseif ($t == 43) {
+  echo "in a fast geman car";
+} elseif ($t == 44) {
+  echo "i'm amazed that i survived";
+} elseif ($t == 45) {
+  echo "as it closes up my arms";
+} elseif ($t == 46) {
+  echo "the position of the worms";
+} elseif ($t == 47) {
+  echo "an airbag saved my life";
+} elseif ($t == 48) {
+  echo "up up through my heart";
+} elseif ($t == 49) {
+  echo "into my brain";
+} elseif ($t == 50) {
+  echo "yeahhh";
+} else {
+  echo "see that long and stretchy smile go right across your faaace";
+};
+?>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<button onClick="window.location.reload();">sit down, stand up</button>
+</body>
+</html>
